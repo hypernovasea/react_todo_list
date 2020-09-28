5fd18f1ddb9254561ff7e5b27f6e8c8be0492688
selveus1 <fs031090@gmail.com>
2020-09-28 11:25:29 -0400

    removed some commented out code

--- src/App.js
+++ src/App.js
@@ -14,22 +14,6 @@ class App extends Component {
         data: null
     }
 
-    // componentDidMount() {
-    //     this.callBackendAPI()
-    //         .then(res => this.setState({ data: res.express }))
-    //         .catch(err => console.log(err));
-    // }
-
-    // callBackendAPI = async () => {
-    //     const response = await fetch('/express_backend');
-    //     const body = await response.json();
-
-    //     if (response.status !== 200) {
-    //         throw Error(body.message)
-    //     }
-    //     return body;
-    // }
-
     componentDidMount() {
         this.fetchTodos()
             .then(res => this.setState({ items: res.tasks }))
@@ -108,17 +92,6 @@ class App extends Component {
             </div>
         );
     }
-    // render() {
-    //     return (
-    //       <div className="App">
-    //         <header className="App-header">
-    //           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-    //           <h1 className="App-title">Welcome to React</h1>
-    //         </header>
-    //         <p className="App-intro">{this.state.data}</p>
-    //       </div>
-    //     );
-    // }
 
 }
 
